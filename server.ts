7778a6f0fa8061f1fb42381128f92d7d63bede64
Will Farhat <willf668@gmail.com>
2022-06-13 01:04:17 -0400

    ts conversion

---
+++ server.ts
@@ -0,0 +1,65 @@
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+if (!fs.existsSync('./saveData')) fs.mkdirSync('./saveData');
+const saveJSON = (data: object, fileName: string) => {
+    fs.writeFile(fileName, JSON.stringify(data), (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+};
+const loadJSON = (fileName: string, defaultData = {}) => {
+    if (!fs.existsSync(fileName)) {
+        saveJSON(defaultData, fileName);
+        return defaultData;
+    }
+    return JSON.parse(fs.readFileSync(fileName).toString()) || {};
+};
+
+const users = loadJSON('./saveData/users.json');
+
+app.use('*', (req, res, next) => {
+    const uid = req.headers.authorization || '';
+    req.headers.user = users[uid] || null;
+    next();
+});
+
+app.use('/progress*', (req, res, next) => {
+    const user = req.headers.user;
+    if (!user) return res.status(201).send({ error: 'Not logged in' });
+    next();
+});
+
+app.get('/progress*', (req, res) => {
+    const { host, pathname } = req.query;
+    res.send({
+        progress: 'intro',
+    });
+});
+
+app.post('/progress', (req, res) => {
+    console.log(req.body);
+    res.send({
+        progress: '100',
+    });
+});
+
+app.post('/settings', (req, res) => {
+    const { settings } = req.body;
+    return res.status(200).send({});
+});
+
+app.use('*', (req, res) => {
+    res.status(404).send('Not found');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
+});
